2cc8ce00f384e726daf55031fc52a24453de01c9
Brent Timmermans <brenttimmermans7@gmail.com>
2024-01-22 10:13:47 +0100

    🔧 add MUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { Button, ButtonGroup } from '@mui/material'
+
 export default function Home() {
-  return <main>this is the landing page</main>
+  return (
+    <main>
+      this is the landing page
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+    </main>
+  )
 }
