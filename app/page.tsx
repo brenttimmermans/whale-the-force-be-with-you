5a0e9142fb65d55e94e420c454c78dc69e38a1a1
Brent Timmermans <brenttimmermans7@gmail.com>
2024-01-22 10:47:49 +0100

    ✨ initial overview page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,35 @@
-import { Button, ButtonGroup } from '@mui/material'
+import { Card, Grid } from '@mui/material'
+import Image from 'next/image'
+import { getAllCharacters } from './lib/data'
+import { Character } from './types'
 
-export default function Home() {
+export default async function Home() {
+  const characters = await getAllCharacters()
+
   return (
     <main>
-      this is the landing page
-      <ButtonGroup
-        variant="contained"
-        aria-label="outlined primary button group"
-      >
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
+      <Grid container spacing={3}>
+        {characters.map(character => (
+          <Grid item sm key={character.id}>
+            <CharacterCard character={character} />
+          </Grid>
+        ))}
+      </Grid>
     </main>
+  )
+}
+
+const CharacterCard = ({ character }: { character: Character }) => {
+  return (
+    <Card>
+      <Image
+        src={character.image}
+        alt={`Image of ${character.name}`}
+        width={200}
+        height={300}
+        style={{ objectFit: 'cover' }}
+      />
+      <div>{character.name}</div>
+    </Card>
   )
 }
