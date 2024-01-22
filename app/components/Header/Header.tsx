ffb59c84289689813e74b388d7705f52a5fb123a
Brent Timmermans <brenttimmermans7@gmail.com>
2024-01-22 13:17:45 +0100

    ✨ add Header

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header>
+      <Link href="/">
+        <h1>whale-code-test</h1>
+      </Link>
+    </header>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import StoreProvider from '@/app/StoreProvider'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Header from './components/Header/Header'
 import './globals.css'
 
 import '@/app/styles/normalize.css'
@@ -22,7 +23,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <StoreProvider>
+          <AppRouterCacheProvider>
+            <Header />
+            {children}
           <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+          </AppRouterCacheProvider>
         </StoreProvider>
       </body>
     </html>
