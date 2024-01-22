0567a1e005b72aa1f58270b756c09b5b29bf59e5
Brent Timmermans <brenttimmermans7@gmail.com>
2024-01-22 11:51:40 +0100

    ✨ introduce redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import StoreProvider from '@/app/StoreProvider'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        <StoreProvider>
+          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        </StoreProvider>
       </body>
     </html>
   )
