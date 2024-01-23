1bec832ecc48bdd6406b6eba6376a3c777ccb2f4
Brent Timmermans <brenttimmermans7@gmail.com>
2024-01-23 08:22:38 +0100

    ✨ add detail error page with redirect

---
+++ app/[id]/error.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Button, Container, Typography } from '@mui/material'
+import Link from 'next/link'
+
+export default function ErrorDetailPage() {
+  return (
+    <Container maxWidth="md" sx={{ textAlign: 'center', margin: '50px 0' }}>
+      <Typography variant="h2" sx={{ fontSize: '1.2rem' }}>
+        Looks like this character doesn&apos;t exist
+      </Typography>
+      <Link href="/" passHref>
+        <Button>Go back home</Button>
+      </Link>
+    </Container>
+  )
+}
