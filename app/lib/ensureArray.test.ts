90eb2250574f634f29e297cb488935dbabad3a05
Brent Timmermans <brenttimmermans7@gmail.com>
2024-01-23 08:53:04 +0100

    ✅ add ensureArray tests

---
+++ app/lib/ensureArray.test.ts
@@ -0,0 +1,19 @@
+import { ensureArray } from './ensureArray'
+
+describe(ensureArray, () => {
+  it('should return an array with string', () => {
+    expect(ensureArray('foo')).toEqual(['foo'])
+  })
+
+  it('should return an array with string[]', () => {
+    expect(ensureArray(['foo'])).toEqual(['foo'])
+  })
+
+  it('should return an array with number', () => {
+    expect(ensureArray(3)).toEqual([3])
+  })
+
+  it('should return an array with number[]', () => {
+    expect(ensureArray([3])).toEqual([3])
+  })
+})
