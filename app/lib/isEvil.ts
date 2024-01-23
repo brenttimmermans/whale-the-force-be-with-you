2dbd1677763bb95eb8cc596379b84d53d5ae3de8
Brent Timmermans <brenttimmermans7@gmail.com>
2024-01-23 08:53:41 +0100

    ✨ implement isEvil

---
+++ app/lib/isEvil.ts
@@ -0,0 +1,18 @@
+import { Character } from '../types'
+import { ensureArray } from './ensureArray'
+
+export function isEvil(
+  character: Pick<Character, 'name' | 'affiliations' | 'masters'>,
+) {
+  const nameIsEvil =
+    character.name.includes('Darth') || character.name.includes('Sith')
+  const affiliationAreEvil = character.affiliations.some(
+    affiliation =>
+      affiliation.includes('Sith') || affiliation.includes('Darth'),
+  )
+
+  const masters = ensureArray(character.masters)
+  const masterAreEvil = masters.some(master => master.includes('Darth'))
+
+  return nameIsEvil || affiliationAreEvil || masterAreEvil
+}
