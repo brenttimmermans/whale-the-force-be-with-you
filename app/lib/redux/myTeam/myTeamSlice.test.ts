e5c0fe552291d1417307b2a9d3b12d2a4caf0a1e
Brent Timmermans <brenttimmermans7@gmail.com>
2024-01-23 08:53:13 +0100

    ✅ add myTeamReducer tests

---
+++ app/lib/redux/myTeam/myTeamSlice.test.ts
@@ -0,0 +1,45 @@
+import { Character } from '@/app/types'
+import myTeamReducer, {
+  addCharacterToMyTeam,
+  removeCharacterFromMyTeam,
+} from './myTeamSlice'
+
+describe(myTeamReducer, () => {
+  it('should return the initial state', () => {
+    expect(myTeamReducer(undefined, {} as any)).toEqual({
+      characters: [],
+    })
+  })
+
+  it('should add a character to the team', () => {
+    const character = _createCharacter()
+
+    expect(myTeamReducer(undefined, addCharacterToMyTeam(character))).toEqual({
+      characters: [character],
+    })
+  })
+
+  it('should remove a character from the team', () => {
+    const character = _createCharacter()
+    const previousState = {
+      characters: [character],
+    }
+
+    expect(
+      myTeamReducer(previousState, removeCharacterFromMyTeam(character.id)),
+    ).toEqual({
+      characters: [],
+    })
+  })
+})
+
+function _createCharacter(
+  options: Partial<Character> = {},
+): Pick<Character, 'id' | 'name' | 'image'> {
+  return {
+    id: 1,
+    name: 'Luke Skywalker',
+    image: 'https://foo.bar',
+    ...options,
+  }
+}
